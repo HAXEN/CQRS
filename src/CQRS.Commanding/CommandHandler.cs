56fd5305d731b2ed5204b2d00cd398f07bf3604d
Håkan Alexander <hakan.alexander@gmail.com>
2021-06-08 15:47:24 +0200

    First setup

---
+++ src/CQRS.Commanding/CommandHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace CQRS.Commanding
+{
+    public abstract class CommandHandler
+    {
+    }
+}
---
+++ test/CQRS.Commanding.Tests/CommandHandler_Tests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace CQRS.Commanding.Tests
+{
+    public class CommandHandler_Tests
+    {
+        public class TestCommandHandler : CommandHandler
+        {
+        }
+
+        [Fact]
+        public void Should_be_able_to_Create_CommandHandler()
+        {
+            var handler = new TestCommandHandler();
+            Assert.NotNull(handler);
+        }
+    }
+}
